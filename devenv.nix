258fe923cae5f22c0de7a9a9bd874a399eb153fa
Lukas <info@brainlesslukas.com>
2026-06-06 10:50:07 +0200

    feat: devenv tasks

--- devenv.nix
+++ devenv.nix
@@ -8,5 +8,21 @@
 
   packages = with pkgs; [ 
     platformio 
+    figlet
+    lolcat
   ];
+
+  scripts = {
+    "pio:build".exec = "pio run";
+    "pio:upload".exec = "pio run --target upload";
+    "pio:monitor".exec = "pio device monitor -b 115200 --quiet";
+    "pio:run".exec = "pio run --target upload & pio device monitor -b 115200 --quiet";
+    "pio:clean".exec = "pio run --target clean";
+  };
+
+  enterShell = ''
+    echo
+    figlet -f slant "ctrlPad" | lolcat -p 1
+    echo
+  '';
 }
