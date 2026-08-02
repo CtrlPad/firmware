f303e5f76d9652bc7bd283b45f98bd1677f752b7
Lukas <info@brainlesslukas.com>
2026-08-02 14:47:56 +0200

    fix: copy lv conf next to lvgl folder

--- extra_script.py
+++ extra_script.py
@@ -1,8 +1,12 @@
-import os
+import os, shutil
 Import("env")
 
-# include toolchain paths
 env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=True)
-
-# override compilation DB path
 env.Replace(COMPILATIONDB_PATH=os.path.join("$BUILD_DIR", "compile_commands.json"))
+
+# Copy lv_conf.h next to the lvgl folder
+_src = os.path.join(env.subst("$PROJECT_DIR"), "include", "lv_conf.h")
+_dst_dir = os.path.join(env.subst("$PROJECT_LIBDEPS_DIR"), env["PIOENV"])
+_dst = os.path.join(_dst_dir, "lv_conf.h")
+if os.path.isfile(_src) and os.path.isdir(_dst_dir):
+    shutil.copy2(_src, _dst)
