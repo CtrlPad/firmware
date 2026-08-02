0c77c70c2b16d82072d893f01433a80b46e6cf17
Lukas <info@brainlesslukas.com>
2026-08-02 16:30:50 +0200

    fix: enable coontent

--- extra_script.py
+++ extra_script.py
@@ -4,9 +4,17 @@ Import("env")
 env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=True)
 env.Replace(COMPILATIONDB_PATH=os.path.join("$BUILD_DIR", "compile_commands.json"))
 
-# Copy lv_conf.h next to the lvgl folder
-_src = os.path.join(env.subst("$PROJECT_DIR"), "include", "lv_conf.h")
-_dst_dir = os.path.join(env.subst("$PROJECT_LIBDEPS_DIR"), env["PIOENV"])
-_dst = os.path.join(_dst_dir, "lv_conf.h")
-if os.path.isfile(_src) and os.path.isdir(_dst_dir):
+_include = os.path.join(env.subst("$PROJECT_DIR"), "include")
+_libdeps = os.path.join(env.subst("$PROJECT_LIBDEPS_DIR"), env["PIOENV"])
+
+# Copy lv_conf.h next to the lvgl folder (../../lv_conf.h relative to lvgl/src/)
+_src = os.path.join(_include, "lv_conf.h")
+_dst = os.path.join(_libdeps, "lv_conf.h")
+if os.path.isfile(_src) and os.path.isdir(_libdeps):
+    shutil.copy2(_src, _dst)
+
+# Copy userSetup.h into the TFT_eSPI library as User_Setup.h
+_src = os.path.join(_include, "userSetup.h")
+_dst = os.path.join(_libdeps, "TFT_eSPI", "User_Setup.h")
+if os.path.isfile(_src) and os.path.isdir(os.path.dirname(_dst)):
     shutil.copy2(_src, _dst)
